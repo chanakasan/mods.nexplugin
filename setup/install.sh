a17eebe63f557ff585153ea093d0dff8a1cc3564
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-12 22:44:24 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -11,21 +11,7 @@ main() {
   validate
   remove_from_bashrc
   copy_to_bashrc
-  download_user_mods
   finish
-}
-
-download_user_mods() {
-  local dest=$HOME/user_mods
-  if [ -d "$dest" ]; then
-    mkdir -p $HOME/backup
-    mv $dest $HOME/backup/user_mods--$(timestamp)
-  fi
-  git clone git@github.com:chanakasan/nex_user_mods.git $dest
-}
-
-timestamp() {
-    echo $(date -u +"%Y_%m%d_%H%M_%S")
 }
 
 main
--- src/main.sh
+++ src/main.sh
@@ -1,12 +1,5 @@
-mkdir -p $HOME/user_mods/bash
-touch $HOME/user_mods/bash/empty.sh
-
-for f in $nex_umods_path/bash/*.sh; do
-  source $f
-done
-
-for f in $nex_umods_path/*; do
-  tmp_file=$f/src/main.sh
+for f in $HOME/user_mods/*; do
+  tmp_file=$f/src/entry.sh
   if [ -f "$tmp_file" ]; then
     source $tmp_file
   fi
