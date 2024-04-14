02eed2ded74d95685a9e8ca88628fa782551e642
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-14 11:23:33 +0530

    refac

--- setup/bashrc.tpl
+++
@@ -1,2 +0,0 @@
-export nex_umods_path=__s1__
-source $nex_umods_path/src/main.sh
--- src/main.sh
+++ src/main.sh
@@ -1,4 +1,4 @@
-for f in $HOME/user_mods/*; do
+for f in $nex_mods_base/*; do
   tmp_file=$f/src/entry.sh
   if [ -f "$tmp_file" ]; then
     source $tmp_file
