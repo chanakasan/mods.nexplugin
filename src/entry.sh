6905a5c87d13c204c48e2f80d79fe21001c6e8f7
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-14 22:29:32 +0530

    rev

--- src/entry.sh
+++ src/entry.sh
@@ -1,7 +1,10 @@
+set -e
+
 source $(nex _config)
-for f in $nex_mods_base/*; do
+
+for f in $nex_mod_base/*; do
   tmp_file=$f/src/entry.sh
   if [ -f "$tmp_file" ]; then
-    source $tmp_file
+     source $tmp_file
   fi
 done
